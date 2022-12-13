e81959ed7c7a77fbf459f00cb4ef824f1099497f
Jordan Harband <ljharb@gmail.com>
2022-12-13 12:07:41 -0800

    [patch] add an additional criteria

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var test = {
 	foo: {}
 };
 
+var $Object = Object;
+
 module.exports = function hasProto() {
-	return { __proto__: test }.foo === test.foo;
+	return { __proto__: test }.foo === test.foo && !({ __proto__: null } instanceof $Object);
 };
