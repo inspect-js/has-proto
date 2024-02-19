64352626cf511c0276d5f4bb6be770a0bf0f8524
Jordan Harband <ljharb@gmail.com>
2024-02-19 12:29:01 -0800

    add types

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var test = {
 
 var $Object = Object;
 
+/** @type {import('.')} */
 module.exports = function hasProto() {
+	// @ts-expect-error: TS errors on an inherited property for some reason
 	return { __proto__: test }.foo === test.foo
 		&& !(test instanceof $Object);
 };
