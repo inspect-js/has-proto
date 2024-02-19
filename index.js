d1f1a4bdc135f115a10f148ce302676224534702
Jordan Harband <ljharb@gmail.com>
2024-02-19 12:28:09 -0800

    [Refactor] tiny cleanup

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var test = {
+	__proto__: null,
 	foo: {}
 };
 
 var $Object = Object;
 
 module.exports = function hasProto() {
-	return { __proto__: test }.foo === test.foo && !({ __proto__: null } instanceof $Object);
+	return { __proto__: test }.foo === test.foo
+		&& !(test instanceof $Object);
 };
